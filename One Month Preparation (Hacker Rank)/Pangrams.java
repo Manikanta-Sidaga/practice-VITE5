5a7ee378f04998372552549c1e4c3d923f4e0479
Manikanta-Sidaga <123537553+Manikanta-Sidaga@users.noreply.github.com>
2023-02-02 21:19:14 +0530

    Pangrams.java(Hacker rank)

---
+++ One Month Preparation (Hacker Rank)/Pangrams.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+    HashMap<Character,Integer> map= new HashMap<>();
+    int count=0;
+    s=s.toLowerCase();
+    for(int i=0;i<s.length();i++){
+        if(!map.containsKey(s.charAt(i)) && (s.charAt(i)>='a') && s.charAt(i)<='z'){
+            map.put(s.charAt(i),1);
+            count+=1;
+        }
+    }
+    if(count==26){
+        return "pangram";
+    }
+    else{
+        return "not pangram";
+    }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
